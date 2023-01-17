6a316b6726a7ecc7ef5ffed2b3f2c02c2b4cdd2e
kaBeech <contact@kaBeech.com>
2023-01-17 14:25:15 -0800

    Update models scaffolding

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+import cards from "./card.ts";
+import cardTranslations from "./cardTrasnslation.ts";
+import languages from "./cardTrasnslation.ts";
+import users from "./user.ts";
+
+export default {
+  cards,
+  cardTranslations,
+  languages,
+  users,
+};
