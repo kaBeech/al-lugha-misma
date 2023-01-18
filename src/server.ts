cde8d34f9be5ea1bf65dca1fda10c49f72f68545
kaBeech <contact@kaBeech.com>
2023-01-17 17:06:57 -0800

    Add example working templating on home page

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import { Application, Context } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-// import {
-//   etaEngine,
-//   oakAdapter,
-//   viewEngine,
-// } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts";
+import {
+  etaEngine,
+  oakAdapter,
+  viewEngine,
+} from "https://deno.land/x/view_engine@v10.5.1c/mod.ts";
 
 import models from "./models/index.ts";
 import routes from "./routes/index.ts";
 
-const port = parseInt(config()["PORT"]);
+// For some reason setting port with dotenv is breaking the view engine
+// const port = parseInt(config()["PORT"]);
+const port = 8000;
+
 const app = new Application();
 
 const logging = async (ctx: Context, next: Function) => {
@@ -39,14 +42,14 @@ app.addEventListener("listen", () => {
   console.log(`Listening on localhost:${port}`);
 });
 
-// app.use(
-//   viewEngine(oakAdapter, etaEngine, {
-//     viewRoot: "./views/eta",
-//   }),
-// );
+app.use(
+  viewEngine(oakAdapter, etaEngine, {
+    viewRoot: "./views/eta",
+  }),
+);
 
-// app.use(async (ctx, next) => {
-//   ctx.render("index.eta", { name: "John" });
-// });
+app.use(async (ctx, next) => {
+  ctx.render("index.eta", { name: "an early version of my web app!" });
+});
 
 await app.listen({ port });
