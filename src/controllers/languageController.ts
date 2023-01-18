b754badaff5dc51b1827e7b57c959368ccd46cea
kaBeech <contact@kaBeech.com>
2023-01-17 16:54:05 -0800

    Try to get templating to work

--- src/controllers/languageController.ts
+++ src/controllers/languageController.ts
@@ -1,10 +1,17 @@
 import { Context } from "https://deno.land/x/oak/mod.ts";
+// import { compileFileClient } from "https://deno.land/x/pug/mod.ts";
 // import models from "../models/index.ts";
 
 const language_list = (ctx: Context, next: Function) => {
   //   const list_language = models.languages;
 
   //   ctx.response.render("language_list", {
+  //     title: "Language List",
+  //     language_list: list_language,
+  //   });
+
+  //   const compiledFunction = compileFileClient("../views/language.pug");
+  //   ctx.response.body = compiledFunction({
   //     title: "Language List",
   //     language_list: list_language,
   //   });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import { Application, Context } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
+// import {
+//   etaEngine,
+//   oakAdapter,
+//   viewEngine,
+// } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts";
 
 import models from "./models/index.ts";
 import routes from "./routes/index.ts";
@@ -33,5 +38,15 @@ app.use(routes.challenges.routes());
 app.addEventListener("listen", () => {
   console.log(`Listening on localhost:${port}`);
 });
+
+// app.use(
+//   viewEngine(oakAdapter, etaEngine, {
+//     viewRoot: "./views/eta",
+//   }),
+// );
+
+// app.use(async (ctx, next) => {
+//   ctx.render("index.eta", { name: "John" });
+// });
 
 await app.listen({ port });
