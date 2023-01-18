9ae892fb8fa62c0ca64c729722b45e2acfe49fe0
kaBeech <contact@kaBeech.com>
2023-01-17 21:11:17 -0800

    Cleanup

--- src/controllers/languageController.ts
+++ src/controllers/languageController.ts
@@ -1,5 +1,4 @@
 import { Context } from "https://deno.land/x/oak/mod.ts";
-// import models from "../models/index.ts";
 
 import { configure, renderFile } from "https://deno.land/x/eta@v1.11.0/mod.ts";
 
@@ -10,19 +9,6 @@ configure({
 });
 
 const language_list = async (ctx: Context, next: Function) => {
-  // const list_language = ctx.state.models.languages.values();
-
-  // ctx.render("language_list.eta", {
-  //   title: "Language List",
-  //   language_list: list_language,
-  // });
-
-  //   const compiledFunction = compileFileClient("../views/language.pug");
-  //   ctx.response.body = compiledFunction({
-  //     title: "Language List",
-  //     language_list: list_language,
-  //   });
-
   const list_language = ctx.state.models.languages.values();
 
   const templateResult = await renderFile("language_list.eta", {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
 import { Application, Context } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-// import {
-//   etaEngine,
-//   oakAdapter,
-//   viewEngine,
-// } from "https://deno.land/x/view_engine@v10.5.1c/mod.ts";
-import { configure, renderFile } from "https://deno.land/x/eta/mod.ts";
 
 import models from "./models/index.ts";
 import routes from "./routes/index.ts";
@@ -43,22 +37,5 @@ app.use(routes.challenges.routes());
 app.addEventListener("listen", () => {
   console.log(`Listening on localhost:${port}`);
 });
-
-// app.use(
-//   viewEngine(oakAdapter, etaEngine, {
-//     viewRoot: "./views/eta",
-//   }),
-// );
-
-// app.use((ctx, next) => {
-//   ctx.render("index.eta", { name: "an early version of my web app!" });
-// });
-
-// const viewPath = `${Deno.cwd()}/views/eta/`
-
-// configure({ views: viewPath });
-// const getName = (u: URL) =>
-//   u.searchParams.get("name") || "an early version of my web app!";
-// let templateResult = await renderFile("index.eta", { name: "an early version of my web app!" });
 
 await app.listen({ port });
