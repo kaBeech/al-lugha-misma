fa982179c0bfecaf0ef4bc65095b1903fad6b85d
kaBeech <contact@kaBeech.com>
2023-01-21 11:31:04 -0800

    Messily make postgres work

--- src/controllers/languageController.ts
+++ src/controllers/languageController.ts
@@ -1,21 +1,46 @@
 import { Context } from "https://deno.land/x/oak/mod.ts";
 import { configure, renderFile } from "https://deno.land/x/eta@v1.11.0/mod.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const viewPath = `${Deno.cwd()}/views/`;
+// const databaseUrl = config()["DATABASE_URL"];
+const databaseUrl = "dburl";
 
-configure({
-  views: viewPath,
-});
+const pool = new postgres.Pool(databaseUrl, 3, true);
+
+const connection = await pool.connect();
+try {
+  await connection.queryObject`
+    CREATE TABLE IF NOT EXISTS languages (
+      id SERIAL PRIMARY KEY,
+      title TEXT NOT NULL
+    )
+  `;
+} finally {
+  connection.release();
+}
+
+// const viewPath = `${Deno.cwd()}/views/`;
+
+// configure({
+//   views: viewPath,
+// });
 
 const language_list = async (ctx: Context, next: Function) => {
-  const list_language = Array.from(ctx.state.models.languages.values());
+  // const list_language = Array.from(ctx.state.models.languages.values());
+  const list_language = await connection.queryObject`
+  SELECT * FROM languages
+`;
+
+  const list_language_string = JSON.stringify(list_language.rows, null, 2);
 
   const templateResult = await renderFile("language_list.eta", {
     title: "Language List",
-    language_list: list_language,
+    language_list: list_language_string,
   });
 
   ctx.response.body = templateResult;
+  // ctx.response.body = JSON.stringify(list_language.rows, null, 2);
 };
 
 export default {
