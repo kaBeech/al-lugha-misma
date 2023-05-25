a550c0553c7694aa477eb7d861dd11f959929617
kaBeech <contact@kaBeech.com>
2023-05-24 22:04:50 -0700

    Add ORM-free languageController

--- src/controllers/languageControllerPonder.ts
+++ src/controllers/languageControllerPonder.ts
@@ -4,42 +4,27 @@ import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 // import { config } from "https://deno.land/x/dotenv/mod.ts";
 import * as ponder from "https://deno.land/x/ponder@v0.1.0/mod.ts";
 import { load } from "https://deno.land/std@0.189.0/dotenv/mod.ts";
-import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const env = await load();
 
-const config: string = env["PG_CONFIG"];
+const DB_URI = env["DB_URI"] as string;
 
-const client = new Client(config);
-await client.connect();
-
-const list_language_result = await client.queryArray(
-  "SELECT NAME FROM CHALLENGES",
-);
-console.log(list_language_result.rows); // [[1, 'Carlos'], [2, 'John'], ...]
-
-const language_list = list_language_result;
-
-// const object_result = await client.queryObject("SELECT ID, NAME FROM PEOPLE");
-// console.log(object_result.rows); // [{id: 1, name: 'Carlos'}, {id: 2, name: 'John'}, ...]
-await client.end();
-
-// const ponderDB1 = await ponder.poolConnection(DB_URI);
+const ponderDB1 = await ponder.poolConnection(DB_URI);
 
 // const ponderDB1 = await ponder.poolConnection(
 //   "dburi",
 // );
 
-// const language_list = async (ctx: Context, next: Function) => {
-//   // const list_language = Array.from(ctx.state.models.languages.values());
-//   const list_language = await ponderDB1.findAllinOne("languagestest");
-//   // const list_language = await ponder.QueryBuilder.prototype.findRow(
-//   //   "transliterated_words",
-//   //   "reference_word_english",
-//   //   "1",
-//   // );
-//   ctx.response.body = { "language_list": list_language };
-// };
+const language_list = async (ctx: Context, next: Function) => {
+  // const list_language = Array.from(ctx.state.models.languages.values());
+  const list_language = await ponderDB1.findAllinOne("languagestest");
+  // const list_language = await ponder.QueryBuilder.prototype.findRow(
+  //   "transliterated_words",
+  //   "reference_word_english",
+  //   "1",
+  // );
+  ctx.response.body = { "language_list": list_language };
+};
 
 /////
 
@@ -64,16 +49,7 @@ await client.end();
 
 // // configure({
 // //   views: viewPath,
-// // });anguage_list = async (ctx: Context, next: Function) => {
-//   // const list_language = Array.from(ctx.state.models.languages.values());
-//   const list_language = await ponderDB1.findAllinOne("languagestest");
-//   // const list_language = await ponder.QueryBuilder.prototype.findRow(
-//   //   "transliterated_words",
-//   //   "reference_word_english",
-//   //   "1",
-//   // );
-//   ctx.response.body = { "language_list": list_language };
-// };
+// // });
 
 // const language_list = async (ctx: Context, next: Function) => {
 //   // const list_language = Array.from(ctx.state.models.languages.values());
