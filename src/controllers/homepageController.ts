7bba2f57db41782dc1be4875ece493d4493a4cf3
kaBeech <contact@kaBeech.com>
2023-05-25 01:02:39 -0700

    Cleanup

--- src/controllers/homepageController.ts
+++ src/controllers/homepageController.ts
@@ -1,6 +1,6 @@
 import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-const index = (ctx: Context, next: Function) => {
+const index = (ctx: Context) => {
   ctx.response.body = {
     "result": "You have successfully pinged the Al-Lugha-Misma API <3",
   };
--- src/controllers/potatoController.ts
+++ src/controllers/potatoController.ts
@@ -8,7 +8,7 @@ const config: string = env["PG_CONFIG"];
 
 const client = new Client(config);
 
-const potato_list = async (ctx: Context, next: Function) => {
+const potato_list = async (ctx: Context) => {
   await client.connect();
   const list_potato_result = await client.queryObject(
     "SELECT LANGUAGES.LANGUAGE, TRANSLITERATED_WORD FROM LANGUAGES JOIN TRANSLITERATED_WORDS ON (LANGUAGES.ID=TRANSLITERATED_WORDS.LANGUAGE) JOIN REFERENCE_WORDS_ENGLISH ON (REFERENCE_WORD_ENGLISH=REFERENCE_WORDS_ENGLISH.ID) WHERE WORD = 'Potato'",
